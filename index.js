e425a837c9589b450848180598c3d72835356618
Tim <umaroffteymur@yandex.ru>
2024-12-28 06:07:19 +0300

    refactor:add linear serch func

---
+++ index.js
@@ -0,0 +1,14 @@
+/* linear searsh */
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
+
+function linearSearch(array, item) {
+    for (let i = 0; i < array.length; i++){
+        if(array[i] === item){
+            return i; 
+        }
+    }
+    return null;
+}
+
+console.log(linearSearch(array, 8));
